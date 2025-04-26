45b54162ba7369522cd65c065f070d8a60d8a3af
ADRN_NKT <combinerk@gmail.com>
2025-04-26 15:43:35 +0300

    Add TPL examples

---
+++ TPLExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Concurrent;
+
+var results = new ConcurrentBag<int>();
+Parallel.For(0, 100, i =>
+{
+    // Simulate some work
+    Task.Delay(100).Wait();
+    results.Add(i);
+});
+
+Console.WriteLine($"Processed {results.Count} items in parallel.");
+Console.Read();
