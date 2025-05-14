d0dd5a7e63a41cd8a2e955b0ed3ea8864d012cf3
ADRN_NKT <combinerk@gmail.com>
2025-05-14 18:18:40 +0300

    Add Task and Value task bench

---
+++ TaskVsValueTask/GetAsValueTask.cs
@@ -0,0 +1,116 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace TaskVsValueTask
+{
+
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class GetAsValueTask
+    {
+
+        public static Dictionary<Guid, string> Storage { get; set; } = new Dictionary<Guid, string>();
+
+        public static List<string> ToSearch = new List<string>();
+
+        private static string _path = $"{Environment.CurrentDirectory}valueTask.txt";
+
+        public GetAsValueTask()
+        {
+            var guids = Utils.GetGuids(300_000);
+
+            var text = guids.Select(g => g.ToString()).Aggregate((a, b) => $"{a}\n{b}");
+
+            Utils.WriteSampleAsync(text, _path);
+
+            foreach(var i in guids)
+            {
+                if(Storage.Count <= 500 && !Storage.ContainsKey(i))
+                {
+                    var rnd = new Random();
+             
+                    Storage.TryAdd(guids.ToArray()[rnd.Next(0, guids.Count() - 1)], i.ToString());
+                }
+                if (ToSearch.Count < 500)
+                {
+                    var rnd = new Random();
+
+                    ToSearch.Add(guids.ToArray()[rnd.Next(0, guids.Count() - 1)].ToString());
+                }
+                if (Storage.Count >= 500)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static async Task<bool> GetValue(string searchValue)
+        {
+            using var file = new FileStream(_path, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true);
+            
+            byte[] buffer = new byte[0x100];
+            
+            int numRead;
+
+            string result = null;
+
+            while ((numRead = await file.ReadAsync(buffer, 0, buffer.Length)) != 0)
+            {
+                string text = Encoding.UTF8.GetString(buffer, 0, numRead);
+
+                if (searchValue == text)
+                {
+                    result = text;
+                    break;
+                }
+            }
+
+            return !string.IsNullOrWhiteSpace(result); 
+        }
+
+        [Benchmark]
+        public async Task<bool> Main()
+        {
+            Task<bool> result = null;
+
+            foreach(var i in ToSearch)
+            {
+                if(Storage.TryGetValue(Guid.Parse(i), out var value))
+                {
+                    result = Task.FromResult(true);
+                }
+                else
+                {
+                    result =  GetValue(i);
+                }  
+            }
+
+            return await result;
+        }
+
+        [Benchmark]
+        public async ValueTask<bool> MainValueTask()
+        {
+            Task<bool> result = Task.FromResult(true);
+            bool valueResult = false;
+
+            foreach (var i in ToSearch)
+            {
+                if (Storage.TryGetValue(Guid.Parse(i), out var value))
+                {
+                    valueResult = true;
+                }
+                else
+                {
+                    result = GetValue(i);
+                }
+            }
+
+            return valueResult ? valueResult : await result;
+        }
+    }
+}
---
+++ TaskVsValueTask/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskVsValueTask
+{
+    public static class Utils
+    {
+        public static IEnumerable<Guid> GetGuids(int count = 10_000) => Enumerable.Range(0, count).Select(i => Guid.NewGuid()).AsEnumerable();
+
+        public static void WriteSampleAsync(string text, string path = null)
+        {
+            if (path == null)
+            {
+                path = Environment.CurrentDirectory + "txt.txt";
+            }
+
+            if (File.Exists(path)) File.Delete(path);
+
+            using var fs = new FileStream(path, FileMode.CreateNew, FileAccess.Write, FileShare.None, 1);
+
+            var textBytes = Encoding.UTF8.GetBytes(text);
+
+            fs.Write(textBytes, 0, textBytes.Length);
+        }
+    }
+}
