7ae2efd8c9233f197235e91da3d2deb78cd0455a
ADRN_NKT <combinerk@gmail.com>
2025-05-14 18:53:30 +0300

    Add ThreadPool example

---
+++ ThreadPoolInation/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Main thread");
+for(var i = 0;i < 10000; i++)
+{
+    ThreadPool.QueueUserWorkItem(BackgroundWork);
+}
+Thread.Sleep(1000);
+Console.WriteLine("End of main");
+
+void BackgroundWork(object parameter)
+{
+    Console.WriteLine("Background thread");
+}
