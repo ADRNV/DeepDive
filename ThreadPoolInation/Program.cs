47d7e37fb9b064465a5a15d34593186e75e7b3de
ADRN_NKT <combinerk@gmail.com>
2025-05-14 22:30:22 +0300

    Execute tasks instead Threads

--- ThreadPoolInation/Program.cs
+++ ThreadPoolInation/Program.cs
@@ -1,12 +1,27 @@
 ﻿Console.WriteLine("Main thread");
-for(var i = 0;i < 10000; i++)
-{
-    ThreadPool.QueueUserWorkItem(BackgroundWork);
-}
-Thread.Sleep(1000);
-Console.WriteLine("End of main");
+Console.WriteLine(ThreadPool.ThreadCount);
+var tasks = new List<Task>();
 
-void BackgroundWork(object parameter)
+for(var i = 0;i <= 100; i++)
 {
-    Console.WriteLine("Background thread");
+    var copy = i;
+    var task = Task.Run(() => BackgroundWork(copy));
+    tasks.Add(task);
+}
+Thread.Sleep(5_000);
+
+Task.WaitAll(tasks);
+
+Console.WriteLine($"End of [{Thread.CurrentThread.Name}] [{Thread.CurrentThread.ManagedThreadId}]");
+Console.WriteLine(ThreadPool.ThreadCount);
+
+async Task<int> BackgroundWork(int arg)
+{
+    var miliseconds = new Random().Next(1_000, 3_000);
+
+    Console.WriteLine($"Passed {arg}: Execution at {miliseconds} on {Thread.CurrentThread.ManagedThreadId}[{Thread.CurrentThread.Name}]");
+    
+    await Task.Delay(miliseconds);
+
+    return await Task.FromResult(0);
 }
