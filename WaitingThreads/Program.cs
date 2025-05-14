39895f2935ee6896c279676e3b1e43f959bb1abf
ADRN_NKT <combinerk@gmail.com>
2025-05-14 18:18:24 +0300

    Add thread example

---
+++ WaitingThreads/Program.cs
@@ -0,0 +1,30 @@
+﻿var threads = new Thread[150];
+
+for(int i = 0;i < threads.Length; i++)
+{
+    threads[i] = new Thread(SomeWork);
+    threads[i].Start();
+}
+
+foreach(var thread in threads)
+{
+    var terminated = thread.Join(TimeSpan.FromSeconds(10));
+
+    if (terminated)
+    {
+        Console.WriteLine($"[{thread.ManagedThreadId}] - Terminated");
+    }
+    else
+    {
+        Console.WriteLine($"[{thread.ManagedThreadId}] - Waited");
+    }
+    //thread.Join();
+}
+
+Console.WriteLine("Finish");
+
+void SomeWork(){
+    var randomTime = new Random().Next(3000, 15000);
+    Thread.Sleep(randomTime);
+    Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Work at {randomTime}");
+}
