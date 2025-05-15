084f4f704914ad4937d6d9ef6e406f368e684134
ADRN_NKT <combinerk@gmail.com>
2025-05-15 20:02:01 +0300

    Add locks

---
+++ SharedThereadData/Program.cs
@@ -0,0 +1,29 @@
+﻿var locker = new Lock();
+
+void GetIncorrectValue()
+{
+    int theValue = 0;
+    var threads = new Thread[100];
+
+    for (int i = 0; i < threads.Length; ++i)
+    {
+        threads[i] = new Thread(() =>
+        {
+            for (int j = 0; j < 100_000; ++j)
+            {
+                lock (locker)
+                {
+                  ++theValue;
+                }
+            }           
+        });
+        threads[i].Start();
+    }
+    foreach (var current in threads)
+    {
+        current.Join();
+    }
+    Console.WriteLine(theValue);
+}
+
+GetIncorrectValue();
