c51f7673d798270c4daecb9af6e19f12005f6972
ADRN_NKT <combinerk@gmail.com>
2025-05-23 19:52:59 +0300

    Add async & await in multithreading

---
+++ ThreadPoolInation/NetworkQueringWithTasks.cs
@@ -0,0 +1,63 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ThreadPoolInation
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class NetworkQueringWithTasks
+    {
+        private HttpClient _httpClient;
+
+        public Dictionary<string, string> Cache = new Dictionary<string, string>();
+
+        public List<KeyValuePair<string, string>> History = new();
+
+        public NetworkQueringWithTasks()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://icanhazdadjoke.com");
+        }
+
+        public async Task<string> GetJoke(string query)
+        {
+            if(Cache.TryGetValue(query, out var value))
+            {
+                History.Add(new KeyValuePair<string, string>(query, value));
+                return value;
+            }
+
+            var result = (await _httpClient.GetAsync($"j/{query}"));
+
+            Cache[query] = result.ToString();
+
+            History.Add(new KeyValuePair<string, string>(query, result.ToString()));
+            
+            return result.ToString();
+        }
+
+        public Thread RunFromThread()
+        {
+            var thread = new Thread(async () =>
+            {
+                var result = await GetJoke(new Random().Next(1, 5).ToString());
+              
+                Out(result);
+
+            });
+            thread.Name = "Network fetch";
+
+            return thread;
+        }
+
+        private void Out(string toOut)
+        {
+            Console.WriteLine(toOut);
+        }
+    }
+}
--- ThreadPoolInation/Program.cs
+++ ThreadPoolInation/Program.cs
@@ -1,27 +1,22 @@
-﻿Console.WriteLine("Main thread");
-Console.WriteLine(ThreadPool.ThreadCount);
-var tasks = new List<Task>();
+﻿using ThreadPoolInation;
 
-for(var i = 0;i <= 100; i++)
+var example = new NetworkQueringWithTasks();
+var threads = Enumerable.Range(0, 35).Select(i => example.RunFromThread());
+
+foreach(var thread in threads)
 {
-    var copy = i;
-    var task = Task.Run(() => BackgroundWork(copy));
-    tasks.Add(task);
+    thread.Start();
 }
-Thread.Sleep(5_000);
 
-Task.WaitAll(tasks);
-
-Console.WriteLine($"End of [{Thread.CurrentThread.Name}] [{Thread.CurrentThread.ManagedThreadId}]");
-Console.WriteLine(ThreadPool.ThreadCount);
-
-async Task<int> BackgroundWork(int arg)
+Thread.Sleep(10_000);
+Console.WriteLine("Finish");
+foreach (var item in example._cache)
 {
-    var miliseconds = new Random().Next(1_000, 3_000);
-
-    Console.WriteLine($"Passed {arg}: Execution at {miliseconds} on {Thread.CurrentThread.ManagedThreadId}[{Thread.CurrentThread.Name}]");
-    
-    await Task.Delay(miliseconds);
-
-    return await Task.FromResult(0);
+    Console.WriteLine($"{item.Key}:{item.Value.Length}");
 }
+Console.WriteLine("Hitory");
+foreach (var item in example.History)
+{
+    Console.WriteLine($"{item.Key}:{item.Value?.Length}");
+}
+
