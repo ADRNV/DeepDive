d3edadd18b47ec30b7d2c3cb949e33d92cb12846
ADRN_NKT <combinerk@gmail.com>
2025-06-13 17:29:57 +0300

    Add results model

---
+++ LocalClient/Models/RunReport.cs
@@ -0,0 +1,11 @@
+﻿namespace LocalClient.Models
+{
+    public class RunReport(int connectionsCount, int failuresCount, long elapsedMs)
+    {
+        public int ConnectionsCount { get => connectionsCount; }
+
+        public int FailuresCount { get => failuresCount; }
+
+        public long ElapsedMs { get => elapsedMs; }
+    }
+}
