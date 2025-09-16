d7e17e23ad5536fba05f06f7cf18f31bffeaea9e
DESKTOP-IOHGBT9\AnyHDD <combinerk@gmail.com>
2025-09-16 19:55:46 +0300

    Add custom SynchronizationCtx implementation

---
+++ CustomSynchronizationContext/CustomSynchroniztionContext.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Concurrent;
+
+public class SingleThreadSynchronizationContext : SynchronizationContext, IDisposable
+{
+    private readonly BlockingCollection<(SendOrPostCallback, object?)> _queue = new();
+    private readonly Thread _thread;
+    private bool _disposed;
+
+    public SingleThreadSynchronizationContext(string threadName = "SingleThread")
+    {
+        _thread = new Thread(RunOnThread)
+        {
+            Name = threadName,
+            IsBackground = true
+        };
+
+        _thread.Start();
+    }
+
+    private void RunOnThread()
+    {
+        SetSynchronizationContext(this);
+
+        while (!_disposed)
+        {
+            try
+            {
+                var (callback, state) = _queue.Take();
+                callback(state);
+            }
+            catch (OperationCanceledException ex)
+            {
+                break;
+            }
+        }
+    }
+
+    public override void Post(SendOrPostCallback d, object? state)
+    {
+        if (_disposed) return;
+        _queue.Add((d, state));
+    }
+
+    public override void Send(SendOrPostCallback d, object? state)
+    {
+        if (_disposed) return;
+
+        var tcs = new TaskCompletionSource(state);
+        Post(_ =>
+        {
+            try
+            {
+                d(state);
+                tcs.SetResult();
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
+        }, null);
+
+        tcs.Task.Wait(); // Блокируем пока не выполнится
+    }
+
+    public void Dispose()
+    {
+        _disposed = true;
+        _queue.CompleteAdding();
+    }
+}
---
+++ CustomSynchronizationContext/Program.cs
@@ -0,0 +1,27 @@
+﻿
+var customCtx = new SingleThreadSynchronizationContext("customCtx");
+
+//Thread.Sleep(1_000);
+
+var work = Task.Run(async () =>
+{
+    Console.WriteLine($"Task on thread {Environment.CurrentManagedThreadId}\r\n");
+    Console.WriteLine($"Task on thread {Environment.CurrentManagedThreadId}\r\n");
+    return Task.FromResult(42);
+});
+
+foreach (var i in Enumerable.Range(1, 1_000)) 
+{
+    work.ContinueWith((t) =>
+    {
+        // Task in Single thread
+        customCtx.Post(_ =>
+        {
+            Console.WriteLine($"Task {i} on thread {Environment.CurrentManagedThreadId}\r\n");
+        }, null);
+    });
+
+    //await work.ContinueWith((t, s) => Console.WriteLine($"Task {i} on thread {Environment.CurrentManagedThreadId}\r\n"), TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Current);
+}
+
+Console.ReadLine();
