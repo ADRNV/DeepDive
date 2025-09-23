5c1406e7bea72993fec308333fc599e1aa5eeb49
DESKTOP-IOHGBT9\AnyHDD <combinerk@gmail.com>
2025-09-23 19:08:40 +0300

    Fix ctx switching

--- UiAvatingExample/Form1.cs
+++ UiAvatingExample/Form1.cs
@@ -16,7 +16,6 @@ public partial class Form1 : Form
 
     private async void button1_Click(object sender, EventArgs e)
     {
-        await Task.Delay(1_000).ConfigureAwait(false);
         textBox1.Text = await GetTextAsync();
     }
 
