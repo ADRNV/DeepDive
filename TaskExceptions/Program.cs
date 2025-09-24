8c400aff7978a0d3028bde00d2454a272a1c2a40
DESKTOP-IOHGBT9\AnyHDD <combinerk@gmail.com>
2025-09-24 19:32:33 +0300

    Add exceptions (Lov-leve C#) analysis (Rider | SharpLab)

---
+++ TaskExceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿//View decompiled Reder or https://sharplab.io/
+#region Compiled try
+//on decompiled try 
+
+// await File.ReadAllBytesAsync($"{Environment.CurrentDirectory}/File.txt");
+//
+// throw new InvalidOperationException("Sample");//From Main stack
+
+//on decompiled try 
+
+//on decompiled catch and re-throw
+#endregion
+
+#region Continue with
+
+// await File.ReadAllBytesAsync($"{Environment.CurrentDirectory}/File.txt")
+// .ContinueWith((t) =>
+// {
+//     throw new InvalidOperationException("Sample");//From delegate callstack
+// });
+
+#endregion
